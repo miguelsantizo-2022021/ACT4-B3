380c2246d10d3a3e93860f7ddacadbe560eaf1d3
MiguelSantizo <msantizo-2022021@kinal.edu.gt>
2026-07-16 19:50:30 -0600

    refactor: se cambio la configuracion y client.ts para la entrega

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,30 +1,25 @@
-import https from 'node:https';
-
-export function fetchExternalUsers(): Promise<any[]> {
+export async function fetchExternalUsers(): Promise<any[]> {
   const url = 'https://jsonplaceholder.typicode.com/users';
   const apiStart = performance.now();
-  const agent = new https.Agent({ rejectUnauthorized: false });
-  return new Promise((resolve, reject) => {
-    https.get(url, { agent }, (res) => {
-      if (res.statusCode !== 200) {
-        reject(new Error(`HTTP_ERROR: Servidor externo respondió con estado ${res.statusCode}`));
-        return;
-      }
-      let buffer = '';
-      res.on('data', (chunk) => {
-        buffer += chunk;
-      });
-      res.on('end', () => {
-        const apiEnd = performance.now();
-        console.log(`-> Petición HTTPS completada en: ${(apiEnd - apiStart).toFixed(2)} ms`);
-        try {
-          resolve(JSON.parse(buffer));
-        } catch (e) {
-          reject(new Error('PARSING_ERROR: JSON mal formado recibido de la API'));
-        }
-      });
-    }).on('error', (err) => {
-      reject(err);
-    });
-  });
+
+  try {
+    const response = await fetch(url);
+
+
+    if (!response.ok) {
+      throw new Error(`HTTP_ERROR: Servidor externo respondió con estado ${response.status}`);
+    }
+
+    const data = await response.json();
+    const apiEnd = performance.now();
+    
+    console.log(`-> Petición Fetch completada en: ${(apiEnd - apiStart).toFixed(2)} ms`);
+    return data;
+  } catch (error: any) {
+    if (error.message && error.message.startsWith('HTTP_ERROR')) {
+      throw error;
+    }
+  
+    throw new Error(`CONNECTION_ERROR: Fallo de conexión o DNS (${error.message})`);
+  }
 }
